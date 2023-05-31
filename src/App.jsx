63428a41ddb44514007b398c260e1251d802dac0
Juan Pablo Miranda <jpablomiranda.21@gmail.com>
2023-05-31 20:38:41 -0300

    agrego titulo de presentacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Saludo from './componentes/Saludo'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
-      
+      <Saludo></Saludo>
     </>
   )
 }
